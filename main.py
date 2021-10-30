49411a9909124bac0acbe7ad2917b225f2ad4b2b
fatemeata <fa23.atayi@gmail.com>
2021-10-30 17:16:38 +0200

    adding initial Generator File

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import matplotlib.pyplot as plt
-from pattern import Circle,Checkers,Spectrum
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pattern import Circle, Checkers, Spectrum
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     ch = Checkers(250, 25)
