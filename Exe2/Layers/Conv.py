61b970a71837fd8f080da902ed82be9a6a123243
fatemeata <fa23.atayi@gmail.com>
2021-12-05 11:45:00 +0100

    conv layer: 1D input-1D conv filter

--- Exe2/Layers/Conv.py
+++ Exe2/Layers/Conv.py
@@ -1,28 +1,93 @@
 from Layers.Base import BaseLayer
+from Layers.Initializers import UniformRandom, Xavier, He, Constant
+import numpy as np
+from scipy.signal import correlate,convolve
 # TODO: implement Convolution layer class
+
 
 class Conv(BaseLayer):
     def __init__(self, str_shape, conv_shape, num_kernels):
         self.stride_shape = str_shape
         self.convolution_shape = conv_shape
-        self.num_kernels: num_kernels
+        self.num_kernels = num_kernels
         self.trainable = True
+        self.weights = np.random.uniform(0, 1, (num_kernels, *conv_shape))
+        # print("weight shape: ", self.weights.shape)
+        self.bias = np.random.uniform(0, 1, (num_kernels, 1))
+        self.channel = 0
+        # weight dimension: [num_kernel, channel, conv_dim, conv_dim]
 
     def forward(self, input_tensor):
         """
 
-        :param input_tensor:
-        input_tensor- with dim:
+        :param input_tensor
+        dim: [b: batch_size, c: channels, y, x (spatial dim)] - [b, c, y, x]
 
         :return:
         output_tensor- providing the tensor for next layer
-        dim:
+        dim: [b, k: number of kernels, y, x]
         """
 
-        pass
+        if len(input_tensor.shape) == 3:  # input tensor is 1D
+            batch, ch, n_x = input_tensor.shape
+            self.channel = ch
+            f_x = self.convolution_shape[1]
+            pad_x = int(f_x / 2)
+            # Check whether the kernel size is even or not
+            if f_x % 2 == 0:
+                n_x = int(((n_x - f_x) + (2 * pad_x) - 1) / self.stride_shape[0]) + 1
+            else:
+                n_x = int(((n_x - f_x) + (2 * pad_x)) / self.stride_shape[0]) + 1
+            out = np.zeros((batch, self.num_kernels, n_x))
+            for i in range(batch):
+                x = input_tensor[i, :]
+                for k in range(self.num_kernels):
+                    for c in range(self.channel):
+                        x_corr = correlate(x[c, :], self.weights[k, c, :], mode='same')
+                        # print("x correlation: ", x_corr)
+                        # print("x correlation slice: ", x_corr[::2])
+
+                        # The basic slice syntax is i:j:k where i is the starting index,
+                        # j is the stopping index, and k is the step.
+                        out[i, k, :] += x_corr[::self.stride_shape[0]]
+            return out
+
+        else:
+            batch, ch, n_x, n_y = input_tensor.shape
+            # f_y = conv_filter_height, f_x: conv_filter_width
+            f_x = self.convolution_shape[1]
+            f_y = self.convolution_shape[2]
+
+            pad_x = int(f_x / 2)
+            pad_y = int(f_y / 2)
+
+            if f_x % 2 == 0:  # if kernel size is even
+                n_x = int(((n_x - f_x) + (2 * pad_x) - 1) / self.stride_shape[0]) + 1
+            else:
+                n_x = int(((n_x - f_x) + (2 * pad_x)) / self.stride_shape[0]) + 1
+
+            if f_y % 2 == 0:  # if kernel size is even
+                n_y = int(((n_y - f_y) + (2 * pad_y) - 1) / self.stride_shape[1]) + 1
+            else:
+                n_y = int(((n_y - f_y) + (2 * pad_y)) / self.stride_shape[1]) + 1
+
+            out = np.zeros((batch, self.num_kernels, n_x, n_y))  # result
+            input_tensor = np.pad(input_tensor, ((0, 0), (0, 0), (pad_x, pad_x), (pad_y, pad_y-1))) # pad the input tensor
+            print("input tensor shape: ", input_tensor.shape)
+            for i in range(batch):
+                x = input_tensor[i,:]
+                print("x shape: ", x.shape)
+                for k in range(self.num_kernels):
+                    for c in range(self.channel):
+                        pass
+            return out
 
     def backward(self, error_tensor):
         pass
 
     def initialize(self, w_init, b_init):
-        pass
+        fan_in = np.prod(self.convolution_shape)
+        fan_out = self.num_kernels * np.prod(self.convolution_shape[1:])
+        self.weights = w_init.initialize(self.weights.shape, fan_in, fan_out)
+        self.bias = b_init.initialize(self.bias.shape, fan_in, fan_out)
+
